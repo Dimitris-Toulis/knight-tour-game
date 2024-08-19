50b677f2c9f2ce48c68bb00cf688dcfe91bece34
Dimitris-Toulis <dimitritouli@gmail.com>
2024-08-19 16:33:45 +0300

    add button component

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,1 @@
+<button on:click class="bg-blue-500 px-3 py-2 rounded-md text-white"><slot></slot></button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import Button from "$lib/components/button.svelte";
+</script>
+
+<main class="p-3">
+</main>
