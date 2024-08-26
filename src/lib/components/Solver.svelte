a7589f5a915d4c8fa1c88d48a618dac075c88e76
Dimitris-Toulis <dimitritouli@gmail.com>
2024-08-26 19:40:36 +0300

    solver: add spiner and error handling

--- src/lib/components/Solver.svelte
+++ src/lib/components/Solver.svelte
@@ -13,6 +13,8 @@
 	export let freezeGrid: boolean;
 
 	let slowmo = false;
+	let solving = false;
+	let error: string = "";
 
 	let initialized = false;
 	let _solve: (
@@ -29,43 +31,51 @@
 		return (_solve = solve);
 	}
 	async function solveClick() {
-		const solve = await initialize();
-		const solution = await solve(
-			structuredClone(grid),
-			structuredClone(dimensions),
-			structuredClone(moves),
-			lastTile
-		);
-		if (solution) {
-			solverUsed = true;
-			if (slowmo) {
-				showModal = false;
-				freezeGrid = true;
-				for (let i = counter; i <= dimensions.x * dimensions.y; i++) {
-					if (!slowmo) {
-						grid = Array.from(solution);
-						lastTile = grid.indexOf(dimensions.x * dimensions.y);
-						counter = dimensions.x * dimensions.y + 1;
-						break;
+		solving = true;
+		try {
+			const solve = await initialize();
+			const solution = await solve(
+				structuredClone(grid),
+				structuredClone(dimensions),
+				structuredClone(moves),
+				lastTile
+			);
+			if (solution) {
+				solverUsed = true;
+				if (slowmo) {
+					showModal = false;
+					freezeGrid = true;
+					for (let i = counter; i <= dimensions.x * dimensions.y; i++) {
+						if (!slowmo) {
+							grid = Array.from(solution);
+							lastTile = grid.indexOf(dimensions.x * dimensions.y);
+							counter = dimensions.x * dimensions.y + 1;
+							break;
+						}
+						// check if restarted
+						if (i > 1 && lastTile == -1) {
+							break;
+						}
+						const next = solution.indexOf(i);
+						grid[next] = i;
+						lastTile = next;
+						counter++;
+						await new Promise((res) => setTimeout(res, 100));
 					}
-					// check if restarted
-					if (i > 1 && lastTile == -1) {
-						break;
-					}
-					const next = solution.indexOf(i);
-					grid[next] = i;
-					lastTile = next;
-					counter++;
-					await new Promise((res) => setTimeout(res, 100));
+					freezeGrid = false;
+				} else {
+					grid = Array.from(solution);
+					lastTile = grid.indexOf(dimensions.x * dimensions.y);
+					counter = dimensions.x * dimensions.y + 1;
 				}
-				freezeGrid = false;
-			} else {
-				grid = Array.from(solution);
-				lastTile = grid.indexOf(dimensions.x * dimensions.y);
-				counter = dimensions.x * dimensions.y + 1;
-			}
-		} else alert("No solution!");
-		showModal = false;
+			} else alert("No solution!");
+			showModal = false;
+			solving = false;
+		} catch (e) {
+			solving = false;
+			if (e instanceof Error) error = e.message;
+			else error = JSON.stringify(e);
+		}
 	}
 </script>
 
@@ -80,6 +90,14 @@
 		<input type="checkbox" name="slowmo" id="slowmo" bind:checked={slowmo} />
 		<label for="slowmo">Reveal solution step-by-step</label>
 	</div>
+	<div class="flex justify-center place-items-center my-4" class:!hidden={!solving}>
+		<div
+			class="border-t-white border-r-white border-b-slate-7 border-l-slate-7 rounded-full aspect-ratio-square w-20 border-3 animate-spin"
+		></div>
+	</div>
+	{#if error != ""}
+		<p class="text-red-6">Error: {error}</p>
+	{/if}
 	<div class="flex justify-center my-3 gap-3">
 		<Button on:click={solveClick} slot="buttons">Solve!</Button>
 		<Button on:click={() => (showModal = false)} slot="buttons">Close</Button>
