e7a04d1c3dc483c80f3d00cdcf2c8556828fa9cb
Dimitris-Toulis <dimitritouli@gmail.com>
2024-08-29 21:10:21 +0300

    fix tile bugs

--- src/components/tile.svelte
+++ src/components/tile.svelte
@@ -8,12 +8,12 @@
 
 <button
 	on:click
-	class="aspect-ratio-square flex place-items-center justify-center font-400 overflow-y-hidden bg-white dark:bg-slate-900"
+	class="aspect-ratio-square overflow-visible bg-white dark:bg-slate-900"
 	class:!bg-blue-600={selected}
 	class:!bg-green-600={candidate}
 	class:text-white={selected || candidate}
 	aria-label="{x},{y}"
 	tabindex={candidate ? 0 : -1}
 >
-	<span>{num == 0 ? "" : num}</span>
+	{num == 0 ? "" : num}
 </button>
