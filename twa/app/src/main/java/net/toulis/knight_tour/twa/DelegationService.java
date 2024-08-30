bcd3dfedff8c67cfe2a850356a56d053ed3e4002
Dimitris-Toulis <dimitritouli@gmail.com>
2024-08-30 19:06:21 +0300

    make twa

---
+++ twa/app/src/main/java/net/toulis/knight_tour/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package net.toulis.knight_tour.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
